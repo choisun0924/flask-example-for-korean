156c1f1fd16f91bb1a7b8239570da7f27c07de1b
최선 트라이텍 컴퓨터 <choisun@hafen.co.kr>
2022-07-27 17:24:56 +0900

    테스트용 json 파일과 main.py에 자주 사용할 예제 코드 추가

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+# pip install flask
+# Flask : 서버
+# render_template : 쉽게 html을 return 할 수 있게 돕는 메서드 (Server Side Rendering 방식)
+# jsonify : 쉽게 str을 json으로 return 할 수 있게 돕는 메서드
+from flask import Flask, render_template, jsonify
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 테스트용 json파일을 열어주고 타입을 확인하면 str인 것을 알 수 있습니다.
+testJson = open('for_test.json', "r+")
+thisOne = testJson.read()
+testJson.close()
+print(thisOne)
+print(type(thisOne))
 
+# 플라스크 Class를 app으로 생성
+app = Flask(__name__)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 함수를 만들고 return 합니다.
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
+@app.route('/test')
+def test():
+    return f'test'
 
-# Press the green button in the gutter to run the script.
+@app.route('/json_test')
+def json_test():
+    # str이 json으로 변경되어 return 됩니다.
+    return jsonify(thisOne)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # app.run 메서드는 아래와 같습니다.
+    '''
+    def run(
+        self,
+        host: t.Optional[str] = None,
+        port: t.Optional[int] = None,
+        debug: t.Optional[bool] = None,
+        load_dotenv: bool = True,
+        **options: t.Any,
+    )
+    '''
+    # 예시
+    # app.run(host='localhost', port=5001, debug=True)
